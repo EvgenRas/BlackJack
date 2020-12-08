865ad792b747fa9da9cfe379f79bd68101ef6c3f
Evgeniy Rassolov <tazij@yandex.ru>
2020-12-08 19:50:25 +0300

    game BlackJack

---
+++ game.h
@@ -0,0 +1,23 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include "deck.h"
+#include "house.h"
+#include "player.h"
+
+using namespace std;
+
+class Game
+{
+public:
+    Game(const vector<string>& names);
+    ~Game();
+    // проводит игру в Blackjack
+    void Play();
+private:
+    Deck m_Deck;
+    House m_House;
+    vector<Player> m_Players;
+};
+
+#endif // GAME_H
